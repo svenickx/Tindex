43135290cc7e2d0d590913a0739147a267742b81
Sven Dockx <sven.dockx@gmail.com>
2022-12-11 17:58:27 +0100

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import MatchesScreen from '../screens/MatchesScreen';
+
+const Stack = createNativeStackNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Matches" component={MatchesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,80 @@
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text, TextInput, TouchableOpacity} from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const LoginScreen = ({navigation}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameLengthValid, setUsernameLengthValid] = useState(false);
+  const [passwordLengthValid, setPasswordLengthValid] = useState(false);
+
+  const apiCall = async () => {
+    if (!usernameLengthValid || !passwordLengthValid) {
+      return;
+    }
+
+    await axios
+      .post('https://login.hikkary.com/users/login', {
+        username,
+        password,
+      })
+      .then(async res => {
+        try {
+          await AsyncStorage.setItem('jwtToken', res.headers['x-access-token']);
+        } catch (err) {
+          console.log(err);
+          return;
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    navigation.navigate('Home');
+  };
+
+  // const apiCall = async () => {
+  //   await axios
+  //     .get('http://mobile.svenckx.com/people')
+  //     .then(res => {
+  //       console.log(res.data);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // };
+
+  useEffect(() => {
+    if (username.length >= 3) {
+      setUsernameLengthValid(true);
+    } else {
+      setUsernameLengthValid(false);
+    }
+    if (password.length >= 8) {
+      setPasswordLengthValid(true);
+    } else {
+      setPasswordLengthValid(false);
+    }
+  }, [username, password]);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        onChangeText={text => setUsername(text)}
+        placeholder="Username"
+      />
+      {!usernameLengthValid && <Text>Minimum 3 caractères</Text>}
+      <TextInput
+        onChangeText={text => setPassword(text)}
+        placeholder="Password"
+      />
+      {!passwordLengthValid && <Text>Minimum 8 caractères</Text>}
+      <TouchableOpacity onPress={() => apiCall()}>
+        <Text>Log In</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
